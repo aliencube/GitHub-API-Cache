0f02befc57d2c8569dec51887fbca8ed2c1b895b
Justin Yoo <justin.yoo@aliencube.com>
2014-07-13 18:37:33 +1000

    Updated web.config

--- SourceCodes/GitHub.Cache.WebApi/js/variables.js
+++ SourceCodes/GitHub.Cache.WebApi/js/variables.js
@@ -1,6 +1,6 @@
 ; "use strict";
 
-var authKey = "2650cba9ca98e349ee9aedec383329a39477950c";
+var authKey = "1234567890123456789012345678901234567890";
 var gitHubApiCacheUrl = "https://githubapicache.apphb.com";
 var gitcdn = "https://cdn.rawgit.com/aliencube/GitHub-API-Cache";
 
