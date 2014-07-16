6a0e3deea7708e46775b562b1905ee48819a2259
Justin Yoo <justin.yoo@aliencube.com>
2014-07-16 16:23:52 +1000

    Added test project.

---
+++ SourceCodes/GitHub.Cache.WebApi.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Aliencube.GitHub.Cache.WebApi.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("be8c7be9-a27a-437f-a5c5-334a882c5fd6")]
